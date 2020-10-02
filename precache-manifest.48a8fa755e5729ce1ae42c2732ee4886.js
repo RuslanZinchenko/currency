c7fa920a8a807bed23f91891bffd8ec507944188
RuslanZinchenko <zinchenko.rusya@gmail.com>
2020-10-03 02:41:10 +0300

    Updates

---
+++ precache-manifest.48a8fa755e5729ce1ae42c2732ee4886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05604766df6e9e40425ff86bf3756291",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "c13903b0e320db2fbcbf",
+    "url": "/currency/static/css/main.24c6e89b.chunk.css"
+  },
+  {
+    "revision": "be7a018d0d70c22f9481",
+    "url": "/currency/static/js/2.c50ff5b0.chunk.js"
+  },
+  {
+    "revision": "c13903b0e320db2fbcbf",
+    "url": "/currency/static/js/main.0e43f1fd.chunk.js"
+  },
+  {
+    "revision": "682945edc1532981b2df",
+    "url": "/currency/static/js/runtime~main.c13dde9a.js"
+  },
+  {
+    "revision": "4da2088d60146164409fd71518e73e98",
+    "url": "/currency/static/media/search-black-24dp.4da2088d.svg"
+  }
+]);
